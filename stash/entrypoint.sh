d461c5a4ed3039a9a4532aeca027cdfaebf8752c
Lubos Babjak <lubos@lubos.sk>
2019-04-08 00:28:52 +0200

    Add entrypoint script

--- stash/entrypoint.sh
+++ stash/entrypoint.sh
@@ -9,20 +9,10 @@ IP="$(ip addr show | grep inet | grep -v '127.0.0.1' | head -1 | awk '{ print $2
 HOSTNAME="$(hostname -s)"
 
 # Check, if ElasticSearch config folder is empty:
-if [[ -z "$(ls -A /config/)" ]]; then
-  cp -r /opt/logstash/config/* /config/
-  mkdir -p /config/pipeline
-  # Set node name.
-  sed -i "s|# node\.name: test|node.name: ${HOSTNAME}|" /config/logstash.yml
-  # Set data folder.
-  sed -i "s|# path\.data:|path\.data: /data/|" /config/logstash.yml
-  # Set pipeline config directory.
-  sed -i "s|# path\.config:|path\.config: /config/pipeline/*.conf|" /config/logstash.yml
-  # Set bind address for LS metrics.
-  sed -i "s|# http\.host: \"127\.0\.0\.1\"|http\.host: \""${IP}"\"|" /config/logstash.yml
-  # Set log folder.
-  sed -i "s|# path\.logs:|path\.logs: /logs/|" /config/logstash.yml
-
+if [[ -z "$(ls -A /data/)" ]]; then
+  mkdir /data/generated
+  mkdir /data/metadata
+  mkdir /data/cache
 fi
 
-/opt/logstash/bin/logstash
+/opt/stash/stash-linux
