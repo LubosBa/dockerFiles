b20d66b0ef8fb3126eb98aa15cd70c07e0f607d5
Lubos Babjak <Lubos.Babjak@erstegroup.com>
2020-02-28 22:26:20 +0100

    Added ZK entrypoint script

---
+++ zookeeper/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# This is an entrypoint of Docker Logstash container, it will setup some
+# sane configuration for the container and copy over the configuration files.
+# Created by Lubos Babjak.
+
+# Get IP address of container.
+IP="$(ip addr show | grep inet | grep -v '127.0.0.1' | head -1 | awk '{ print $2}' |  awk -F'/' '{print $1}')"
+# Get hostname
+HOSTNAME="$(hostname -s)"
+
+# Check, if ElasticSearch config folder is empty:
+if [[ -z "$(ls -A /data/)" ]]; then
+  mkdir /data/generated
+  mkdir /data/metadata
+  mkdir /data/cache
+fi
+
+while : 
+do
+    echo "Hello!"
+    sleep 30
+done
