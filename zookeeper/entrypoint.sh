5764a6c172ff755cc066911d5360f0f09a2d17da
Lubos Babjak <Lubos.Babjak@erstegroup.com>
2020-02-28 22:46:11 +0100

    Adjusted env of ZK Docker file.

--- zookeeper/entrypoint.sh
+++ zookeeper/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# This is an entrypoint of Docker Logstash container, it will setup some
+# This is an entrypoint of Docker ZooKeeper container, it will setup some
 # sane configuration for the container and copy over the configuration files.
 # Created by Lubos Babjak.
 
@@ -8,15 +8,14 @@ IP="$(ip addr show | grep inet | grep -v '127.0.0.1' | head -1 | awk '{ print $2
 # Get hostname
 HOSTNAME="$(hostname -s)"
 
-# Check, if ElasticSearch config folder is empty:
-if [[ -z "$(ls -A /data/)" ]]; then
-  mkdir /data/generated
-  mkdir /data/metadata
-  mkdir /data/cache
+# Check, if ZooKeeper config folder is empty:
+if [[ -z "$(ls -A /conf/)" ]]; then
+    cp -r /opt/apache-zookeeper-3.5.7-bin/conf/ /conf/
 fi
 
 while : 
 do
     echo "Hello!"
+    which java
     sleep 30
 done
