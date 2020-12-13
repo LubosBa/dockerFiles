55de4022f10afb23d9cb0595057a035e16f00c53
Lubos Babjak <lubos@lubos.sk>
2020-12-13 18:00:52 +0100

    Upgraded Kafka to 2.6.0

--- kafka/entrypoint.sh
+++ kafka/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# This is an entrypoint of Docker ZooKeeper container, it will setup some
+# This is an entrypoint of Docker Kafka container, it will setup some
 # sane configuration for the container and copy over the configuration files.
 # Created by Lubos Babjak.
 
@@ -8,9 +8,9 @@ IP="$(ip addr show | grep inet | grep -v '127.0.0.1' | head -1 | awk '{ print $2
 # Get hostname
 HOSTNAME="$(hostname -s)"
 
-# Check, if ZooKeeper config folder is empty:
+# Check, if Kafka config folder is empty:
 if [[ -z "$(ls -A /config/)" ]]; then
-    cp -r /opt/kafka_2.12-2.4.0/config/* /config/
+    cp -r /opt/kafka_2.13-2.6.0/config/* /config/
 fi
 
 if [[ ! -f /config/kafka-server.properties ]]
@@ -19,4 +19,4 @@ then
     exit 1
 fi
 
-/opt/kafka_2.12-2.4.0/bin/kafka-server-start.sh /config/kafka-server.properties
+/opt/kafka_2.13-2.6.0/bin/kafka-server-start.sh /config/kafka-server.properties
