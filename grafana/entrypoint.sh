34e913bf9226185e8fbddd5e83936dbb05ee29b2
root <root@krabica.m8.sk>
2021-11-11 23:04:37 +0100

    Upgraded to Grafana 8.2.3

--- grafana/entrypoint.sh
+++ grafana/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-# This is an entrypoint of Docker Prometheus container, it will setup some
+# This is an entrypoint of Docker Grafana container, it will setup some
 # sane configuration for the container and copy over the configuration files.
 # Created by Lubos Babjak.
 
 # Get IP address of container.
-IP="$(ip addr show | grep inet | grep -v '127.0.0.1' | head -1 | awk '{ print $2}' |  awk -F'/' '{print $1}')"
+#IP="$(ip addr show | grep inet | grep -v '127.0.0.1' | head -1 | awk '{ print $2}' |  awk -F'/' '{print $1}')"
 # Get hostname
-HOSTNAME="$(hostname -s)"
+#HOSTNAME="$(hostname -s)"
 
 # Check, if Grafana config folder is empty:
 if [[ -z "$(ls -A /config/)" ]]; then
